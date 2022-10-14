d883773caa98e02668ae1532be117dedf10749a8
Shayenne Gail Galiste <70041031+shaygail@users.noreply.github.com>
2022-10-14 21:17:35 +1300

    Update Map.jsx

--- components/Map.jsx
+++ components/Map.jsx
@@ -10,7 +10,7 @@ const Map = (props) => {
 				<AspectRatio ratio={16 / 9}>
 					<Box
 						as="iframe"
-						src="//massey.maps.arcgis.com/apps/Embed/index.html?webmap=88685d3e83a34e7bb198a1d66bcd762a&extent=158.302,-47.4784,180,-33.2365&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&legend=true&disable_scroll=true&theme=light"
+						src="https://www.google.com/maps/d/embed?mid=1yPs4eTny-QyFoAvJsiiRloYY2kPnmJM&ehbc=2E312F"
 						alt="demo"
 						borderRadius="lg"
 					/>
