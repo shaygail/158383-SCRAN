53962a87eab0b5eef9f299e5b2e6dff60260084f
Shayenne Gail Galiste <70041031+shaygail@users.noreply.github.com>
2022-10-14 17:06:47 +1300

    Add files via upload

--- components/NewsFeed.jsx
+++ components/NewsFeed.jsx
@@ -10,7 +10,7 @@ const NewsFeed = (props) => {
 				<SimpleGrid columns={[1, 2]} spacing={4} h="xl">
 					<Box
 						as="iframe"
-						src="https://rss.app/embed/v1/feed/_TaqOVIln8c9y3xXr"
+						src="https://rss.app/feeds/nK4AKSjGcTL4dhvK.xml"
 						alt="twitter feed"
 						borderRadius="lg"
 						w="full"
